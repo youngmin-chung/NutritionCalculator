6957b3dce30575a1308daa7d7707f057b1fd257b
Youngmin Chung <dev.youngmin@gmail.com>
2021-12-15 12:00:27 -0500

    Create View pages for home and data

--- NutritionCalculator/Controllers/DataController.cs
+++ NutritionCalculator/Controllers/DataController.cs
@@ -2,15 +2,34 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using NutritionCalculator.Models;
 
 namespace NutritionCalculator.Controllers
 {
     public class DataController : Controller
     {
+        AppDbContext _ctx;
+        // an instance of the context is passed in via the controller’s constructor
+        public DataController(AppDbContext context)
+        {
+            _ctx = context;
+        }
+
         public async Task<IActionResult> Index()
         {
+            UtilityModel util = new UtilityModel(_ctx);
+            string msg = "";
             var json = await getMenuItemJsonFromWebAsync();
-            return Content(json);
+            try
+            {
+                msg = (util.loadNutritionTables(json)) ? "tables loaded" : "problem loading tables";
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+            ViewBag.LoadedMsg = msg;
+            return View();
         }
 
         private async Task<String> getMenuItemJsonFromWebAsync()
---
+++ NutritionCalculator/Models/README.md
@@ -0,0 +1,5 @@
+﻿### ASP.NET MODELS
+
+* It's representation of the data being posted to or received from the Controller
+* The data being worked on ina View or the representation of the domain specific entities operating in the business tier
+* It contains core application information includes data, validation rules, data access and aggregation login
