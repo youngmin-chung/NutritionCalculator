89b64a7e09a33d48b89b7c0335015f2b6cb1c9fb
Youngmin Chung <dev.youngmin@gmail.com>
2021-12-23 17:51:07 -0500

    In tray page, add daily value at Health Canada.

---
+++ NutritionCalculator/Utils/DailyValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionCalculator.Utils
+{
+    public class DailyValues
+    {
+        // place all session variable names for application here
+        public const int Calories = 2000;
+        public const int Carbohydrate = 300;
+        public const int Cholesterol = 300;
+        public const decimal Fat = 65.0M;
+        public const int Fibre = 25;
+        public const int Protein = 0;
+        public const int Sodium = 2400;
+     
+    }
+}
